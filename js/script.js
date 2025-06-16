18c40ac33246617c5e8a0819e112e6ba0923609c
Vincent <vinz2007@hotmail.com>
2025-06-16 18:46:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,15 +11,15 @@ function calculateMean(arr) {
 // get screen width
 function getWidth() {
   if (self.innerWidth) {
-    return self.innerWidth - ;
+    return self.innerWidth - 10;
   }
 
   if (document.documentElement && document.documentElement.clientWidth) {
-    return document.documentElement.clientWidth ;
+    return document.documentElement.clientWidth - 10;
   }
 
   if (document.body) {
-    return document.body.clientWidth ;
+    return document.body.clientWidth - 10;
   }
 }
 
